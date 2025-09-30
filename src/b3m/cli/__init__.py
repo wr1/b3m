0eba33183010e5b1f4ac3b6eecc129a53973988d
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-30 13:25:11 +0200

    Fix import path for b3_msh.cli.cli app in CLI

--- src/b3m/cli/__init__.py
+++ src/b3m/cli/__init__.py
@@ -4,20 +4,20 @@ from pathlib import Path
 # Add paths to sub-project source directories
 sys.path.append(str(Path(__file__).parent.parent.parent / "b3_geo" / "src"))
 sys.path.append(str(Path(__file__).parent.parent.parent / "b3_msh" / "src"))
-sys.path.append(str(Path(__file__).parent.parent.parent / "b3_pln" / "src"))
+sys.path.append(str(Path(__file__).parent.parent.parent / "b3_drp" / "src"))
 
 from treeparse import cli
 from b3_geo.cli import app as geo_app
-from b3_msh.cli.mesh import app as msh_app
-from b3_pln.cli import app as pln_app
+from b3_msh.cli.cli import app as msh_app
+from b3_drp.cli import app as drp_app
 
 # Set names and sort keys for subgroups
 geo_app.name = "geo"
 geo_app.sort_key = 1
 msh_app.name = "msh"
 msh_app.sort_key = 2
-pln_app.name = "pln"
-pln_app.sort_key = 0
+drp_app.name = "drp"
+drp_app.sort_key = 0
 
 super_app = cli(
     name="b3m",
@@ -26,7 +26,7 @@ super_app = cli(
     show_types=True,
     show_defaults=True,
     line_connect=True,
-    subgroups=[geo_app, msh_app, pln_app],
+    subgroups=[geo_app, msh_app, drp_app],
 )
 
 
